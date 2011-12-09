6310cb06e03212e1bcc6a27f35c57d734d0b909f
dsmith03 <dsmith03@4c54da1e-8164-11de-92dd-b1b912a521ea>
2011-12-09 16:33:08 +0000

    added  -server parameter

--- easygsp/bin/start.bat
+++ easygsp/bin/start.bat
@@ -1,2 +1,2 @@
 
-java -Djava.util.logging.config.file=%EASYGSP_HOME%\conf\logging.properties -Deasygsp.home=%EASYGSP_HOME% -Djava.security.manager -Djava.security.policy=file://%EASYGSP_HOME%\conf\easygsp.policy -cp %EASYGSP_HOME%\.;%EASYGSP_HOME%\lib\*;%EASYGSP_HOME%\bin\easygsp.jar -Djava.library.path=%EASYGSP_HOME%\lib\native com.sybrix.easygsp.server.EasyGServer
+java -server -XX:+DisableExplicitGC -Djava.util.logging.config.file=%EASYGSP_HOME%\conf\logging.properties -Deasygsp.home=%EASYGSP_HOME% -Djava.security.manager -Djava.security.policy=file://%EASYGSP_HOME%\conf\easygsp.policy -cp %EASYGSP_HOME%\.;%EASYGSP_HOME%\lib\*;%EASYGSP_HOME%\bin\easygsp.jar -Djava.library.path=%EASYGSP_HOME%\lib\native com.sybrix.easygsp.server.EasyGServer
--- easygsp/bin/start.sh
+++ easygsp/bin/start.sh
@@ -2,7 +2,7 @@
 
 export EASYGSP_HOME=/opt/easygsp
 
-$JAVA_HOME/bin/java -Djava.util.logging.config.file=$EASYGSP_HOME/conf/logging.properties -Deasygsp.home=$EASYGSP_HOME -Djava.security.manager -Djava.security.policy=file:$EASYGSP_HOME/conf/easygsp.policy -cp .:$EASYGSP_HOME/.:$EASYGSP_HOME/lib/*:$EASYGSP_HOME/bin/easygsp.jar -Djava.library.path=$EASYGSP_HOME/lib/native com.sybrix.easygsp.server.EasyGServer
+$JAVA_HOME/bin/java -server -XX:+DisableExplicitGC -Djava.util.logging.config.file=$EASYGSP_HOME/conf/logging.properties -Deasygsp.home=$EASYGSP_HOME -Djava.security.manager -Djava.security.policy=file:$EASYGSP_HOME/conf/easygsp.policy -cp .:$EASYGSP_HOME/.:$EASYGSP_HOME/lib/*:$EASYGSP_HOME/bin/easygsp.jar -Djava.library.path=$EASYGSP_HOME/lib/native com.sybrix.easygsp.server.EasyGServer
 
 
 
