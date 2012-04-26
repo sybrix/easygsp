1046dae8f331ccb47c571bbcb85ba28b6cf2ccb5
dsmith03 <dsmith03@4c54da1e-8164-11de-92dd-b1b912a521ea>
2012-04-26 03:57:36 +0000

    added copyright notice

--- trunk/easygsp/src/com/sybrix/easygsp/http/IncludeTemplateEngine.java
+++ trunk/easygsp/src/com/sybrix/easygsp/http/IncludeTemplateEngine.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2009 the original author or authors.
+ * Copyright 2012. the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 package com.sybrix.easygsp.http;
 
@@ -80,10 +80,6 @@ public class IncludeTemplateEngine extends TemplateEngine {
                 this.groovyShell = groovyShell;
         }
 
-//        public Template createTemplate(Reader reader) throws CompilationFailedException, IOException {
-//                //return createTemplate(reader, null);
-//                return null;
-//        }
 
         public Template createTemplate(Reader reader) throws CompilationFailedException, IOException {
                 SimpleTemplate template = new SimpleTemplate();
@@ -548,8 +544,8 @@ public class IncludeTemplateEngine extends TemplateEngine {
                                 }
 
 
-                                SimpleTemplate template = new SimpleTemplate();
-                                String script = template.parse(new StringReader(contents.toString().replaceAll("\\$", "\\\\\\$")), false, new InheritedTemplateInfo());
+                                SimpleTemplate simpleTemplate = new SimpleTemplate();
+                                String script = simpleTemplate.parse(new StringReader(contents.toString().replaceAll("\\$", "\\\\\\$")), false, new InheritedTemplateInfo());
                                 sw.write(script);
                                 sw.write("rout.print(\"");
 
