42b139a5f884bec541800b13d48c1e40678df874
dsmith03 <dsmith03@4c54da1e-8164-11de-92dd-b1b912a521ea>
2012-04-26 03:57:36 +0000

    added copyright notice

--- easygsp/src/com/sybrix/easygsp/server/EasyGServer.java
+++ easygsp/src/com/sybrix/easygsp/server/EasyGServer.java
@@ -1,17 +1,19 @@
+
+
 /*
- * Copyright 2009 the original author or authors.
+ * Copyright 2012. the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package com.sybrix.easygsp.server;
@@ -52,8 +54,6 @@ public class EasyGServer extends ReceiverAdapter {
         public static JChannel jgroupsChannel;
 
         private ServerSocket serverSocket;
-        //private ExecutorService executorService;
-        private boolean isRunning;
 
         private ConcurrentHashMap<String, ServletContextImpl> applications = new ConcurrentHashMap();
 
@@ -63,7 +63,6 @@ public class EasyGServer extends ReceiverAdapter {
 
         private static volatile boolean stopRequested = false;
 
-        //private static String outputDir;
         private static String serverDir;
         public static String APP_DIR;
         private SessionMonitor sessionMonitor;
